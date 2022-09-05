254dfe33adacac7f3e6ec6a0d8bdc4bafa15d67e
Connor Macdonald <csm119@uclive.ac.nz>
2022-09-05 14:08:19 +1245

    Modified c_lib example to work with new api.

--- examples/c_lib/main.c
+++ examples/c_lib/main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 #include <barracuda_compiler.h>
+
+const char* test_code = "fn fib(n) {\nlet a = 0;\nlet b = 1;\nfor (let i = 0; i < n; i = i + 1) {\nlet temp = a + b;\na = b;\nb = temp;\nprint a;\n}\n}\nfib(10);";
+
 
 int main(int argc, char *argv[]) {
     printf("Testing calling barracuda compiler from a c file.\n");
 
-    // From barracuda_compiler.h
-    hello_world();
+    // Create Request
+    CompilerRequest request;
+    request.code_text = strdup(test_code);
+
+    {
+        // Send Request
+        CompilerResponse response = compile(&request);
+
+        // Process Response
+        printf("Code:\n%s\n\n", test_code);
+        printf("Compiled:\n%s\n", response.code_text);
+        for(int i = 0; i < response.values_list.len; i++) {
+            printf("%f\n", response.values_list.ptr[i]);
+        }
+        // ...
+
+        // Don't forget to free the response
+        free_compile_response(response);
+    }
 }
