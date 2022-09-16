406bd3dcb443d9fdfdcf46bd604e7aa98e1536a4
Connor Macdonald <csm119@uclive.ac.nz>
2022-09-16 22:25:35 +1245

    Added ScopeId Generator + Will generate scope ids as an iterator.

--- src/compiler/ast/scope.rs
+++ src/compiler/ast/scope.rs
@@ -1,7 +1,7 @@
 
 /// Scope Id defines the unique id associated with every scope regardless
 /// of position in the ast tree
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct ScopeId {
     id: u64
 }
@@ -13,9 +13,39 @@ impl ScopeId {
         }
     }
 
+    // Return global scope id: 0
+    pub fn global() -> Self {
+        ScopeId::default()
+    }
+
     pub(super) fn new(id: u64) -> Self {
         ScopeId {
             id
         }
+    }
+
+    pub(super) fn from(&mut self, scope: ScopeId)  {
+        self.id = scope.id;
+    }
+}
+
+pub struct ScopeIdGenerator {
+    previous: ScopeId
+}
+
+impl ScopeIdGenerator {
+    pub fn new() -> Self {
+        Self {
+            previous: ScopeId::default()
+        }
+    }
+}
+
+impl Iterator for ScopeIdGenerator {
+    type Item = ScopeId;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.previous = ScopeId::new(self.previous.id + 1);
+        Some(self.previous.clone())
     }
 }
